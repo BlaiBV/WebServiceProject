4bc5ba2dbf32e49d35f0a24104e3710e7ed108e8
BlaiBV <120587038+BlaiBV@users.noreply.github.com>
2024-04-08 10:25:23 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'WebServiceProject',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
