d72dfc304192b8b67993a7dd5e5133ec6a18d436
Beatriu <BeatriuBadiaSala@gmail.com>
2024-04-16 13:51:21 +0200

    Canvis en la pàgina de home

--- src/app/views/pages/home/home.page.ts
+++ src/app/views/pages/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-//import { register } from 'swiper/element/bundle';
+import { register } from 'swiper/element/bundle';
 
-//register();
+register();
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public fotos: any[] = ['../../../../assets/img/1.jpg', '../../../../assets/img/2.jpg', '../../../../assets/img/3.png'];
+  public pokeball: string = '../../../../assets/img/pokeball.png';
   constructor() { }
 }
